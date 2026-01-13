5cb034475e895db0e43e3bc961d3508614b09a16
Paul David <puddavid@gmail.com>
2026-01-13 16:34:42 -0500

    Deploy as a github site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hebrew-training/',
   plugins: [vue()],
   resolve: {
     alias: {
